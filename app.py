9649cd36e5dd4f77f875d8a2de31169aa4f3a66f
Jonathan Rabbi <jonathanrabbi96@gmail.com>
2023-07-27 10:34:46 +0200

    get post

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, HTTPException
 from pydantic import BaseModel
 from src.XGBoost_pred import evaluate_xgboost_regression
 
@@ -13,11 +13,17 @@ class House_feature(BaseModel):
 
 @app.get("/")
 def activate():
-    return "Welcome, please enter to have your house valued"
+    return ("Welcome, please enter to have your house valued")
 
 @app.post("/predict-house")
 def predict_house_price(data:House_feature):
     data_file_path='/Users/jonathanrabbi/Desktop/Data_Analysis_Immo/Data/Cat_House_details.csv'
-    result=evaluate_xgboost_regression(data_file_path,data)
-    return{"valued at":result}
+    try:
+        result=evaluate_xgboost_regression(data_file_path,data)
+        return{"valued at":result}
+    except ValueError: # if the user does not insert numbers
+        raise HTTPException(status_code=400, detail="please insert numbers.")
+    
+    
+
     
